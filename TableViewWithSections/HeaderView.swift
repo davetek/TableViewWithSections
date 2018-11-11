c2d260897b5d41dd4d07ab0f05b21c52d4ed110e
davetek <dlawrence4994@gmail.com>
2018-11-11 11:16:54 -0500

    v1 b1: Display a table with sections, and include functionality to show section headings in one of three ways: setting the default header section title, or setting a custom view made programmatically, or setting a custom view made with a xib file and associated Swift class.

---
+++ TableViewWithSections/HeaderView.swift
@@ -0,0 +1,26 @@
+//
+//  HeaderView.swift
+//  TableViewWithSections
+//
+//  Created by David Lawrence on 11/11/18.
+//  Copyright © 2018 clarity for action. All rights reserved.
+//
+
+import UIKit
+
+class HeaderView: UITableViewCell {
+    
+    @IBOutlet var headerLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- TableViewWithSections/ViewController.swift
+++ TableViewWithSections/ViewController.swift
@@ -8,12 +8,75 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+let data = [
+    ["London", "Paris", "Berlin"],
+    ["Atlanta", "Boston", "Tucson"],
+    ["Toronto", "Vancouver", "Montreal"]
+]
 
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+        
+    @IBOutlet var tableView: UITableView!
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return data.count
+    }
+    
+//    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+//        return "SECTION \(section)"
+//    }
+    
+//    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+//        let label = UILabel()
+//        label.text = "Header"
+//        label.backgroundColor = UIColor.lightGray
+//        return label
+//    }
+    
+        func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+            let headerView = Bundle.main.loadNibNamed("HeaderView", owner: self, options: nil)?.first as! HeaderView
+    
+            headerView.backgroundColor = UIColor.lightGray
+            headerView.headerLabel.text = "Section \(section)"
+            return headerView
+        }
+    
+//    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+//        return 50
+//    }
+    
+    func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
+        print("will display header view")
+    }
+
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data[section].count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let cityName = data[indexPath.section][indexPath.row]
+        cell.textLabel?.text = "\(cityName)"
+        cell.detailTextLabel?.text = "Section: \(indexPath.section) Row: \(indexPath.row)"
+        return cell
+    }
+    
+
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //table view data will not appear unless the vc is set as tableview datasource
+        tableView.dataSource = self
+        
+        //delegate methods - such as tableView(_:viewForHeaderInSection:) will not appear
+        // unless vc is set as tableView delegate
+        tableView.delegate = self
     }
+    
+    
 
 
 }
